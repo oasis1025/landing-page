dadb5731fdbe3fea7daaac3782aebe682b0f023c
oasis1025 <larryyu1025@gmail.com>
2023-08-02 20:00:15 +0800

    change href

--- components/hero.tsx
+++ components/hero.tsx
@@ -66,7 +66,7 @@ export default function Hero() {
                 <div>
                   <a
                     className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0"
-                    href="#0"
+                    href="https://dev.dynavo.xyz/objects/"
                   >
                     Get Started
                   </a>
@@ -74,7 +74,7 @@ export default function Hero() {
                 <div>
                   <a
                     className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4"
-                    href="#0"
+                    href="https://docs.dynavo.xyz/"
                   >
                     Docs
                   </a>
